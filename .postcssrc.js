1a461393c9dbddf449ea1602fa7190b53f54f364
Alexander Prestmo <aprestmo@gmail.com>
2022-10-11 11:13:56 +0200

    Remove Tailwind and pnpm

--- .postcssrc.js
+++ .postcssrc.js
@@ -2,7 +2,6 @@ module.exports = {
   plugins: [
     require('postcss-nested-ancestors'),
     require('postcss-nested'),
-    require('postcss-import'),
-    require('tailwindcss')
+    require('postcss-import')
   ]
 }
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,4 @@
 module.exports = {
-  plugins: [require('prettier-plugin-tailwindcss')],
   printWidth: 120,
   tabWidth: 2,
   semi: false,
